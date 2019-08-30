4773544c34a6199cb2ec4e1a26893fdb8eac6008
areslee <345558738@qq.com>
2019-08-30 16:36:15 +0800

    注释

--- iconchangedemo/ViewController.m
+++ iconchangedemo/ViewController.m
@@ -17,7 +17,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    UIButton *changeBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    changeBtn.frame = CGRectMake(100, 100, 100, 40);
+    [changeBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [changeBtn setTitle:@"替换icon" forState:UIControlStateNormal];
+    [changeBtn setTitle:@"还原icon" forState:UIControlStateSelected];
+    [changeBtn addTarget:self action:@selector(changeClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:changeBtn];
 }
+
+
+-(void)changeClick:(UIButton *)sender{
+    sender.selected = !sender.isSelected;
+    if (sender.selected == YES) {
+        [[UIApplication sharedApplication] setAlternateIconName:@"iconbear" completionHandler:^(NSError * _Nullable error) {
+            if (error == nil) {
+                NSLog(@"替换成功");
+            }else{
+                NSLog(@"替换失败");
+            }
+        }];
+    }else{
+        [[UIApplication sharedApplication] setAlternateIconName:nil completionHandler:^(NSError * _Nullable error) {
+            if (error == nil) {
+                NSLog(@"还原成功");
+            }else{
+                NSLog(@"还原失败");
+            }
+        }];
+    }
+}
+
+
+
 
 
 @end
